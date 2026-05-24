7de82dba5b495e63ab55396bfc652dcdac68db7f
Nejat Alper Satiroglu <n.satiroglu265@mybvc.ca>
2026-05-23 22:09:21 -0600

    New Input System integrated

--- Assets/_Project/02-Scripts/InputManager.cs
+++ Assets/_Project/02-Scripts/InputManager.cs
@@ -1,29 +1,64 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SocialPlatforms.Impl;
+
 
 
 public class InputManager : MonoBehaviour
 {
-    private Keyboard _keyboard;
+    //private Keyboard _keyboard;
     [SerializeField] private RhythmGrid rhythmGrid;
     [SerializeField] private ScoreManager scoreManager;
     
+    //NewInputActions reference
+    NewInputActions _newInputActions;
+
+    void Awake()
+    {
+        //grabbing the instance
+        _newInputActions = new NewInputActions();
+    }
+
+    /*
     private void Start()
     {
         _keyboard = Keyboard.current;
     }
+     */
+    
+    void OnEnable()
+    {
+        // subscribing to the input events
+        _newInputActions.Enable();
+        
+        _newInputActions.Player.AKey.performed += KeyPressedA;
+        _newInputActions.Player.DKey.performed += KeyPressedD;
+        _newInputActions.Player.GKey.performed += KeyPressedG;
+        _newInputActions.Player.JKey.performed += KeyPressedJ;
+        
+    }
 
-    private void Update()
+    void OnDisable()
+    {
+        // Unsubscribing from input events ondisable
+        _newInputActions.Disable();
+        
+        _newInputActions.Player.AKey.performed -= KeyPressedA;
+        _newInputActions.Player.DKey.performed -= KeyPressedD;
+        _newInputActions.Player.GKey.performed -= KeyPressedG;
+        _newInputActions.Player.JKey.performed -= KeyPressedJ;
+    }
+
+    /*
+     private void Update()
     {
         if (_keyboard != null) // check for null always
         {
-            
+
             if (_keyboard.aKey.wasPressedThisFrame)
             {
                 PressPianoKey(0);
             }
-        
+
             if (_keyboard.dKey.wasPressedThisFrame)
             {
                 PressPianoKey(1);
@@ -33,15 +68,16 @@ public class InputManager : MonoBehaviour
             {
                 PressPianoKey(2);
             }
-        
+
             if (_keyboard.jKey.wasPressedThisFrame)
             {
                 PressPianoKey(3);
             }
-            
-        }
+
+        } 
         
     }
+     */
 
     private void PressPianoKey(int column)
     {
@@ -94,8 +130,32 @@ public class InputManager : MonoBehaviour
                 Destroy(bestNote.gameObject); // destroy the note if it can be played, we need to make sure that the note is destroyed after it is played to prevent it from being scored multiple times
             }
         }
-        
     }
     
+    //keypressed methods
+
+    private void KeyPressedA(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Key pressed A");
+        PressPianoKey(0);
+    }
+
+    private void KeyPressedD(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Key pressed D");
+        PressPianoKey(1);
+    }
+
+    private void KeyPressedG(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Key pressed G");
+        PressPianoKey(2);
+    }
+    private void KeyPressedJ(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Key pressed J");
+        PressPianoKey(3);
+    }
+
 
 }
